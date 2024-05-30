8c818c66e7c81ade43c664eb2303014474a92bf9
diogowagner <diogowagnerms@gmail.com>
2024-05-30 15:09:27 -0300

    settings

--- project/settings.py
+++ project/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    BASE_DIR / 'base_static',
-)
+# STATICFILES_DIRS = (
+#     BASE_DIR / 'base_static',
+# )
 
 STATIC_ROOT = BASE_DIR / 'static'
 
