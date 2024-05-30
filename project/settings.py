254095d47d350e0ac635b9b090868235f3d56a82
diogowagner <diogowagnerms@gmail.com>
2024-05-30 14:55:35 -0300

    settings

--- project/settings.py
+++ project/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    BASE_DIR / 'static',
+    BASE_DIR / 'base_static',
 )
 
 STATIC_ROOT = BASE_DIR / 'static'
@@ -132,8 +132,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-print(BASE_DIR)
 
 try:
     from project.local_settings import *
