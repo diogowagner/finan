e7a6c517538a7202e9c799e06c150dd8438b28b7
diogowagner <diogowagnerms@gmail.com>
2025-01-18 12:15:16 -0300

    melhorias inicio

--- fluxo/views/inicio.py
+++ fluxo/views/inicio.py
@@ -3,11 +3,26 @@ from django.shortcuts import render, redirect
 from fluxo.models import Lancamento
 from finan.models import Conta
 from django.db.models import Sum
+from datetime import date, timedelta
 
 @login_required
 def inicio(request):
 
-    apagar = Lancamento.objects.all().filter(situacao='APAGAR').order_by('data_lancamento')[:10]
+    # Obtém a data de início e fim do mês atual
+    hoje = date.today()
+    data_inicio = date(hoje.year, hoje.month, 1)
+
+    # Calcula a data do primeiro dia do próximo mês
+    if hoje.month == 12:  # Se for dezembro, o próximo mês é janeiro do próximo ano
+        data_fim = date(hoje.year + 1, 1, 1) - timedelta(days=1)
+    else:
+        data_fim = date(hoje.year, hoje.month + 1, 1) - timedelta(days=1)
+        
+    apagar = Lancamento.objects.filter(
+        data_lancamento__range=(data_inicio, data_fim)
+    ).order_by("data_lancamento", "pk").prefetch_related('itens').filter(situacao='APAGAR')
+
+    # apagar = Lancamento.objects.all().filter(situacao='APAGAR').order_by('data_lancamento')[:10]
 
     lista_saldos =[]
     for i in Conta.objects.all():
